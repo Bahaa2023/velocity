852c5ccc542c6de8f1473594acf63491e47e1802
Bahaa2023 <124404177+Bahaa2023@users.noreply.github.com>
2023-07-28 16:01:50 +0200

    mise en ligne intial

--- public/index.php
+++ public/index.php
@@ -2,7 +2,10 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+error_reporting(E_ALL);
+
+ini_set('display_errors', 'On');
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
