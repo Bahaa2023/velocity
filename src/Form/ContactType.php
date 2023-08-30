75d708fba3fe062fafc16b15d6793123f7e48a66
Bahaa2023 <124404177+Bahaa2023@users.noreply.github.com>
2023-08-30 09:56:25 +0200

    added captcha for formulaire de contact

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-// use Gregwar\CaptchaBundle\Type\CaptchaType;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
 
 
 
@@ -22,8 +22,8 @@ class ContactType extends AbstractType
         $builder
             ->add('sujet', TextType::class)
             ->add('email', EmailType::class)
-            ->add('message', TextareaType::class);
-        // ->add('captcha', captchaType::class);
+            ->add('message', TextareaType::class)
+            ->add('captcha', captchaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
